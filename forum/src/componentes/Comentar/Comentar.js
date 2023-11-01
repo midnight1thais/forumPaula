7c3098d7a54815b4b146f42fb88fdd66862f7cc3
Thais <thaisplaneta2017@gmail.com>
2023-11-01 12:21:47 -0300

    Comentários prontos

--- forum/src/componentes/Comentar/Comentar.js
+++ forum/src/componentes/Comentar/Comentar.js
@@ -4,16 +4,60 @@ import { AutorComentario, BotaoCondicional, ComentarioContainer, ComentarioDoAut
 import Curtir from "../Curtir/Curtir";
 
 
-const Comentar = () => {
+const Comentar = ({ postId, comments, autorId }) => {
 
-   
+    const [novoComentario, setNovoComentario] = useState('');
+    const [comentarios, setNovoComentarios] = useState([])
+    const [mostrarComentarios, setMostrarComentarios] = useState(false);
+
+    const adicionarComentario = () => {
+        if (novoComentario.trim() !== '') {
+            createComment(postId, novoComentario);
+            setNovoComentarios(...comments, novoComentario)
+            setNovoComentario(''); // Limpa o input
+        }
+    }
+
+    console.log(autorId)
+    useEffect(() => {
+        adicionarComentario()
+    }, [])
+
+    const novosComentario = comments.map((comentario) => {
+        return (<ComentarioContainer key={comentario.comment_id}>
+            <AutorComentario>{comentario.creator_name}:</AutorComentario>
+            <ComentarioDoAutor>{comentario.comment}</ComentarioDoAutor>
+
+        </ComentarioContainer>)
+    })
+
 
     return (
 
         <>
 
          
-             
+                <ContainerItem>
+
+                <BotaoCondicional onClick={() => setMostrarComentarios(!mostrarComentarios)}>{mostrarComentarios ?('Fechar' ):('Comentar')}</BotaoCondicional>
+                    {mostrarComentarios && ( // Mostrar se mostrarComentarios for true
+                        <ContainerCurtir>
+
+                            <div>
+                            <InputComentar
+                                placeholder="comentario"
+                                value={novoComentario}
+                                onChange={(e) => setNovoComentario(e.target.value)}
+                            />
+                            <button onClick={adicionarComentario}>Enviar</button>
+                            
+                                
+                            </div>
+                            {novosComentario}
+                        </ContainerCurtir>
+                    )}
+                    <Curtir autorId={autorId} />
+                </ContainerItem>
 
             
         </>
